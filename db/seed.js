912a8204196c09b39119ae2902a6f89d8576febd
Bruce Grugett <bgrugett@gmail.com>
2017-05-29 09:52:43 -0400

    added seed

--- db/seed.js
+++ db/seed.js
@@ -1,119 +1,85 @@
 'use strict';
 
-const db = require('./index.js');
- const models = require('./models');
- const User = models.User;
- const Campus = models.Campus;
-// {mapValues} = require('lodash');
+const db = require('../db');
+const User = db.models.user;
+const Campus = db.models.campus;
 
-function seedEverything() {
-  const seeded = {
-    users: users(),
-    campuses: campuses()
-  };
+//db.sync({force: true});
 
-  return Promise.props(seeded);
-}
-
-const users = seed(User, {
-  graceHopper: {
-    email: 'grace@yale.edu',
-    name: 'Grace Hopper'
-  },
-  rocky: {
-    name: 'Rocky',
-    email: 'squirrel@wassU.edu'
-  },
-  bullwinkle: {
-    name: 'Bullwindle',
-    email: 'moose@wassU.edu'
-  },
-  alanTuring: {
-    name: 'Alan Turing',
-    email: 'enigma@cambridge.edu'
-  }
-});
-const campuses = seed(Campus, {
+const campuses = {
   yale: {
     name: 'Yale University',
-    location: 'New Haven CT'
+    location: 'New Haven CT',
+    imageURL: '/images/Yale.jpg'
   },
   wassamotta: {
     name: 'Wassamotta University',
-    lociton: 'Frostbite Falls MN'
+    location: 'Frostbite Falls MN',
+    imageURL: '/images/WossamottaU.jpg'
   },
   cambridge: {
     name: 'Cambridge University',
-    location: 'Cambridge England'
+    location: 'Cambridge England',
+    imageURL: '/images/Cambridge.jpg'
+  },
+  michigan: {
+    name: 'University of Michigan',
+    location: 'Ann Arbor MI',
+    imageURL: '/images/Michigan.jpg'
+  },
+  cornell: {
+    name: 'Cornell University',
+    location: 'Ithica NY',
+    imageURL: '/images/Cornell.jpg'
   }
-});
+};
 
-
-//if (module === require.main) {
-    db.sync({force: true})
-    .then(seedEverything)
-    .finally(() => process.exit(0));
-//}
-
-class BadRow extends Error {
-  constructor(key, row, error) {
-    super(error);
-    this.cause = error;
-    this.row = row;
-    this.key = key;
+const users = {
+  graceHopper: {
+    email: 'grace@yale.edu',
+    name: 'Grace Hopper',
+    campusId: 1
+  },
+  rocky: {
+    name: 'Rocky',
+    email: 'squirrel@wassU.edu',
+    campusId: 2
+  },
+  bullwinkle: {
+    name: 'Bullwindle',
+    email: 'moose@wassU.edu',
+    campusId: 2
+  },
+  alanTuring: {
+    name: 'Alan Turing',
+    email: 'enigma@cambridge.edu',
+    campusId: 3
+  },
+  ford: {
+    name: 'Gerald Ford',
+    email: 'gerry@pouts.gov',
+    campusId: 4
+  },
+  hamilton: {
+    name: 'Margaret Hamilton',
+    email: 'mh@umich.edu',
+    campusId: 4
+  },
+  mandi: {
+    name: 'Mandi M',
+    email: 'mm@cornell.edu',
+    campusId: 5
   }
+};
 
-  toString() {
-    return `[${this.key}] ${this.cause} while creating ${JSON.stringify(this.row, 0, 2)}`;
-  }
-}
-
-// seed(Model: Sequelize.Model, rows: Function|Object) ->
-//   (others?: {...Function|Object}) -> Promise<Seeded>
-//
-// Takes a model and either an Object describing rows to insert,
-// or a function that when called, returns rows to insert. returns
-// a function that will seed the DB when called and resolve with
-// a Promise of the object of all seeded rows.
-//
-// The function form can be used to initialize rows that reference
-// other models.
-function seed(Model, rows) {
-  return (others = {}) => {
-    if (typeof rows === 'function') {
-      rows = Promise.props(
-        mapValues(others,
-          other =>
-            // Is other a function? If so, call it. Otherwise, leave it alone.
-            typeof other === 'function' ? other() : other)
-      ).then(rows);
-    }
-
-    return Promise.resolve(rows)
-      .then(rows => Promise.props(
-        Object.keys(rows)
-          .map(key => {
-            const row = rows[key];
-            return {
-              key,
-              value: Promise.props(row)
-                .then(row => Model.create(row)
-                  .catch(error => { throw new BadRow(key, row, error); })
-                )
-            };
-          }).reduce(
-            (all, one) => Object.assign({}, all, {[one.key]: one.value}),
-            {}
-          )
-        )
+Promise.all(
+      Object.keys(campuses).map(key =>
+      Campus.create(campuses[key])
       )
-      .then(seeded => {
-        console.log(`Seeded ${Object.keys(seeded).length} ${Model.name} OK`);
-        return seeded;
-      }).catch(error => {
-        console.error(`Error seeding ${Model.name}: ${error} \n${error.stack}`);
-      });
-  };
-}
+)
+.then(Object.keys(users).map(key =>
+   User.create(users[key])
+))
+.catch(err => console.error(err));
 
-module.exports = Object.assign(seed, {users, campuses});
+
